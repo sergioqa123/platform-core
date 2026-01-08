856d9656f1af443543b9a27137b9f1004fdec4c9
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-01-08 16:59:47 -0500

    User constructor, getters and setters

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -3,5 +3,35 @@ package domain;
 public class User {
     private String name;
     private String email;
+    private boolean status;
+    private String role;
 
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
