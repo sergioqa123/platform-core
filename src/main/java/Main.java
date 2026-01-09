d8db933f4b6ab053253f6e365c4c16ad331ba137
Sergio <44326862+sergioqa123@users.noreply.github.com>
2026-01-09 12:20:30 -0500

    Updated menu visuals

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,13 +11,14 @@ public class Main {
         UserService userService = new UserService();
 
         do {
-            System.out.println("====== Welcome =====");
-            System.out.println("1. Register user");
-            System.out.println("2. List users");
-            System.out.println("3. Exit");
-            System.out.println("====================");
+            System.out.println("======= Welcome ======");
+            System.out.println("| 1. Register user   |");
+            System.out.println("| 2. List users      |");
+            System.out.println("| 3. Exit            |");
+            System.out.println("======================");
             System.out.print("Choose an option: ");
             option = Integer.parseInt(scanner.nextLine());
+            System.out.println(" ");
             switch (option){
                 case 1:
                     System.out.print("Enter a name: ");
@@ -26,13 +27,15 @@ public class Main {
                     String email = scanner.nextLine();
                     User user = new User(name, email);
                     userService.registerUser(user);
+                    System.out.println(" ");
                     break;
                 case 2:
-                    System.out.println("------- User list -------");
+                    System.out.println("-------------- User list --------------");
                     for (User u : userService.listUsers()) {
                         System.out.println("Name: " + u.getName() + ", Email:" + u.getEmail());
                     }
-                    System.out.println("-------------------------");
+                    System.out.println("---------------------------------------");
+                    System.out.println(" ");
                     break;
             }
 
